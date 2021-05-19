837e4fd3eeb862bd3dd85d87bdd5b1cca9b960a7
Jcheez <jiajunlee2@gmail.com>
2021-05-19 10:58:01 +0800

    5555

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-import asyncio
-import datetime
-import numpy as np
-import aiohttp
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -9,44 +5,9 @@ import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 
+from parallel import result as latest
+from parallel import newresult as historical
 
-links = ["https://api.apify.com/v2/key-value-stores/yaPbKe9e5Et61bl7W/records/LATEST?disableRedirect=true", "https://api.apify.com/v2/datasets/suHgi59tSfu02VsRO/items?limit=16&desc=true"]
-async def main():
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        for l in links:
-            task = asyncio.ensure_future(get_data(l, session))
-            tasks.append(task)
-        data = await asyncio.gather(*tasks)
-    return data
-
-async def get_data(link, session):
-    async with session.get(link) as response:
-        result_data = await response.json()
-        return result_data
-
-data = asyncio.get_event_loop().run_until_complete(main())
-
-result = data[0]
-date = result['lastUpdatedAtApify']
-newdate = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.000Z")    
-result['lastUpdatedAtApify'] = str(newdate)
-
-newresult = data[1]
-newresult.reverse()
-def map_func(dictionary):
-    dictionary["date"] = str(datetime.datetime.strptime(dictionary['lastUpdatedAtApify'], "%Y-%m-%dT%H:%M:%S.000Z").date())
-    return dictionary
-newresult = list(map(map_func, newresult))
-infections = list(np.diff(list(map(lambda x: x['infected'], newresult))))
-infections.insert(0,0)
-counter = 0
-for d in newresult:
-    d["communityCases"] = infections[counter]
-    counter += 1
-
-latest = result
-historical = newresult
 external_stylesheets = [
 {
     'href': 'https://use.fontawesome.com/releases/v5.8.1/css/all.css',
---
+++ parallel.py
@@ -0,0 +1,40 @@
+import asyncio
+import datetime
+import numpy as np
+import aiohttp
+
+
+links = ["https://api.apify.com/v2/key-value-stores/yaPbKe9e5Et61bl7W/records/LATEST?disableRedirect=true", "https://api.apify.com/v2/datasets/suHgi59tSfu02VsRO/items?limit=16&desc=true"]
+async def main():
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for l in links:
+            task = asyncio.ensure_future(get_data(l, session))
+            tasks.append(task)
+        data = await asyncio.gather(*tasks)
+    return data
+
+async def get_data(link, session):
+    async with session.get(link) as response:
+        result_data = await response.json()
+        return result_data
+
+data = asyncio.get_event_loop().run_until_complete(main())
+
+result = data[0]
+date = result['lastUpdatedAtApify']
+newdate = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.000Z")    
+result['lastUpdatedAtApify'] = str(newdate)
+
+newresult = data[1]
+newresult.reverse()
+def map_func(dictionary):
+    dictionary["date"] = str(datetime.datetime.strptime(dictionary['lastUpdatedAtApify'], "%Y-%m-%dT%H:%M:%S.000Z").date())
+    return dictionary
+newresult = list(map(map_func, newresult))
+infections = list(np.diff(list(map(lambda x: x['infected'], newresult))))
+infections.insert(0,0)
+counter = 0
+for d in newresult:
+    d["communityCases"] = infections[counter]
+    counter += 1
